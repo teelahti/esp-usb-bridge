804bf8940e8218f03e3026cb621c62308017f1e8
Tero Teelahti <tero@teelahti.fi>
2026-05-03 19:01:13 +0300

    Add better docs to devenv

--- devenv.nix
+++ devenv.nix
@@ -14,8 +14,8 @@ in
 
   enterShell = ''
     echo "ESP-IDF $(idf.py --version) dev environment ready"
-    echo "  idf.py build       - build the project"
-    echo "  idf.py flash       - flash to device"
-    echo "  picocom /dev/ttyX  - serial monitor"
+    echo "  idf.py build                                    - build the project"
+    echo "  idf.py -p /dev/ttyACM0 flash monitor            - flash to device"
+    echo "  picocom -b 115200 /dev/ttyUSB0 --imap lfcrlf    - serial monitor"
   '';
 }
